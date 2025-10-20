a96fbdf8517f97d6b2da57b8c1bbababdacbf810
Hlxuan <huanglangxuan@hlxuan.top>
2025-10-20 11:29:08 +0800

    update: 新增搬家和加法的项目

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
-<script setup></script>
+<script setup>
+import BanJia from "./components/BanJia.vue"
+import Student from "./components/Student.vue"
+</script>
 
 <template>
-  <div></div>
+  <div>
+    <Student />
+    <BanJia />
+  </div>
 </template>
 
 <style scoped></style>
